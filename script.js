90c4783ab3058d9475e6805e79f02e28ef1390fa
Esa Parkkila <esa.parkkila@tuni.fi>
2025-02-20 11:35:39 +0200

    Added missing try catch block

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 (async () => {
-    const result = await fetch('https://date.nager.at/api/v3/PublicHolidays/2025/FI');
-    const data = await result.json();
+    try {
 
-    console.log('data: ', data);
+        const result = await fetch('https://date.nager.at/api/v3/PublicHolidays/2025/FI');
+        const data = await result.json();
 
-    for (let i = 0; i < data.length; i++) {
+        console.log('data: ', data);
 
-        const holiday = data[i];
+        for (let i = 0; i < data.length; i++) {
 
-        const tbody = document.getElementById('my-tbody');
+            const holiday = data[i];
 
-        const row = document.createElement('tr');
+            const tbody = document.getElementById('my-tbody');
 
-        const cellId = document.createElement('td');
-        cellId.textContent = i+1;
+            const row = document.createElement('tr');
 
-        const nameFiCell = document.createElement('td');
-        nameFiCell.textContent = holiday.localName;
+            const cellId = document.createElement('td');
+            cellId.textContent = i + 1;
+
+            const nameFiCell = document.createElement('td');
+            nameFiCell.textContent = holiday.localName;
 
 
-        const nameEnCell = document.createElement('td');
-        nameEnCell.textContent = holiday.name;
-        const dateCell = document.createElement('td');
-        dateCell.textContent = holiday.date;
+            const nameEnCell = document.createElement('td');
+            nameEnCell.textContent = holiday.name;
+            const dateCell = document.createElement('td');
+            dateCell.textContent = holiday.date;
 
-        // Append columns to row
-        row.appendChild(cellId);
-        row.appendChild(nameFiCell);
-        row.appendChild(nameEnCell);
-        row.appendChild(dateCell);
+            // Append columns to row
+            row.appendChild(cellId);
+            row.appendChild(nameFiCell);
+            row.appendChild(nameEnCell);
+            row.appendChild(dateCell);
 
-        // Append row to table body
-        tbody.appendChild(row);
+            // Append row to table body
+            tbody.appendChild(row);
+
+        }
+    } catch (error) {
+        console.log('error: ', error)
     }
 
 })();
